36bfe9d6773a72f142812b3115041706717409d2
Gyeongrak Choi <peter6617@gmail.com>
2024-06-12 23:31:10 +0900

    Method and Event

--- basics-03-events-starting-code/app.js
+++ basics-03-events-starting-code/app.js
@@ -4,6 +4,16 @@ const app = Vue.createApp({
       counter: 0,
     };
   },
+  methods: {
+    add() {
+      // data property 참조
+      this.counter = this.counter + 1;
+
+    },
+    reduce(){
+      this.counter = this.counter - 1;
+    }
+  }
 });
 
 app.mount('#events');
