a4f302ce805ee02c6ee5d52f101272afb5563da3
Gyeongrak Choi <peter6617@gmail.com>
2024-06-17 23:23:52 +0900

    동적으로 CSS 클래스 추가

--- basics-10-styling-starting-setup/app.js
+++ basics-10-styling-starting-setup/app.js
@@ -16,11 +16,14 @@ const app = Vue.createApp({
     methods: {
         boxSelected(box){
             if(box ==='A'){
-                this.boxAselected = true;                
+                // this.boxAselected = true;                
+                this.boxAselected = !this.boxAselected;// 토글로 수정
             } else if (box === 'B') {
-                this.boxBselected = true;
+                //this.boxBselected = true;
+                this.boxBselected = !this.boxBselected;
             } else if( box === 'C') {
-                this.boxCselected = true;
+                //this.boxCselected = true;
+                this.boxCselected = !this.boxCselected;
             }
         }
     },
